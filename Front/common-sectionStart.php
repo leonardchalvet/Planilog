69fa94a62606749746cbc9025bc6d15fa815e710
Léonard <leonard@agence-me.com>
2018-11-08 11:00:25 -0500

    Init

---
+++ Front/common-sectionStart.php
@@ -0,0 +1,17 @@
+<section id="section-start">
+	<div class="wrapper">
+		<h2>Prêt(e) à vous lancer ?</h2>
+		<p>
+			Commencez dès aujourd’hui ou contactez-nous.
+		</p>
+		<a class="btn" href="">
+			<span class="btn-text">
+				ESSAI GRATUIT
+			</span>
+		</a>
+		<div class="text">
+			ou <a href="">contactez nous</a>
+		</div>
+	</div>
+	<img class="laptop" src="img/common/start-laptop.png" alt="">
+</section>
---
+++ Front/features-mesurer.php
@@ -0,0 +1,180 @@
+<html>
+	<head>
+
+		<title>Feature - Mesurer</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/features.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?> 
+
+		<main class="feature-mesurer">
+			<section id="section-cover">
+				<img class="shape" src="img/home/sectionCover/shape.svg" alt="">
+				<div class="wrapper">
+					<div class="container-text">
+						<h2>MESURER</h2>
+						<h1>
+							La planification sous contraintes pilotée par vos <span>KPI</span>
+						</h1>
+						<p>
+							La stabilité d’un modèle de planification repose sur sa précision et son niveau de détails. Planilog place l’agilité et la flexibilité au centre de ces préoccupations au travers d’un planning de gantt en ligne entièrement personnalisable et collaboratif.  
+						</p>
+						<a class="btn" href="#">
+							<span class="btn-text">ESSAI GRATUIT</span>
+						</a>
+					</div>
+					<div class="container-illu">
+						<video>
+						  <source loop
+						    src="video/home/Mesurer.mp4"
+						    type="video/mp4">
+						</video>
+					</div>
+				</div>
+			</section>
+			<section id="section-features">
+				<div class="wrapper">
+					<div class="container-el">
+						<div class="col-el">
+							<div class="el">
+								<img class="icn" src="img/features/feature-mesurer/icn-visualisation.svg" alt="">
+								<h3>
+									Visualisation Capacité et Charge
+								</h3>
+								<p>
+									Disposez en temps réel d’un visuel graphique permettant de mesurer vos plans de charges sur un horizon libre. Pilotez le taux d’utilisation des ressources (surcharge et sous charge).
+								</p>
+							</div>
+							<div class="el">
+								<img class="icn" src="img/features/feature-mesurer/icn-organisation.svg" alt="">
+								<h3>
+									Organisation des tâches
+								</h3>
+								<p>
+									Tracez et stockez l’ensemble des ordonnancements réalisés et les résultats obtenus. Analysez les tendances et comparez les résultats réels par rapport aux prévisionnels.
+								</p>
+							</div>
+							<div class="el">
+								<img class="icn" src="img/features/feature-mesurer/icn-tableaudebord.svg" alt="">
+								<h3>
+									Tableaux de bord
+								</h3>
+								<p>
+									Visualisez l’intégralité des encours de production et leurs caractéristiques (date lancement, date début, date fin, date limite, niveaux de priorité). Exportez l’ensemble de ces données sur des formats d’analyse ouverts à des outils décisionnels.
+								</p>
+							</div>
+						</div>
+						<div class="col-el">
+							<div class="el">
+								<img class="icn" src="img/features/feature-mesurer/icn-performances.svg" alt="">
+								<h3>
+									Indicateurs de performance supportés par Planilog
+								</h3>
+								<p>
+									<span class="title">Pour la production industrielle : </span>
+									<ul>
+										<li>Nombre d’unités</li>
+										<li>Taux de Rebut</li>
+										<li>Cadences</li>
+										<li>On-Time Delivery OTD</li>
+										<li>Temps de cycles / Turn Around Time (TAT)</li>
+										<li>Taux de rendement Synthétique (TRS) / Overall Equipment </li>
+										<li>Efficiency (OEE)</li>
+										<li>On Time In Full - OTIF</li>
+										<li>Pour la maintenance industrielle : </li>
+										<li>Temps moyen de réparation des machines </li>
+										<li>MTTR</li>
+										<li>Temps moyen entre interventions de maintenance </li>
+										<li>Temps moyen entre pannes MTBF (GMAO)</li>
+										<li>Temps moyen de fonctionnement avant panne MTTF</li>
+									</ul>
+									
+								</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</section>
+			<section id="section-bnd">
+				<div class="background"></div>
+				<div class="wrapper">
+					<div class="container-text">
+						<h2>Voir toutes nos fonctionnalités</h2>
+						<p>
+							Far far away, behind the word mountains, far from the countries Vokalia and Consonantia
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el">
+							<div class="content">
+								<img class="icn" src="img/features/common-features/icn-planifier.svg" alt="">
+								<h3>
+									Planifier
+								</h3>
+								<a class="btn" href="#">
+									<span class="btn-text">DETAILS</span>
+									<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+								</a>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content">
+								<img class="icn" src="img/features/common-features/icn-simuler.svg" alt="">
+								<h3>
+									Simuler
+								</h3>
+								<a class="btn" href="#">
+									<span class="btn-text">DETAILS</span>
+									<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+								</a>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content">
+								<img class="icn" src="img/features/common-features/icn-ordonnancer.svg" alt="">
+								<h3>
+									Ordonnancer
+								</h3>
+								<a class="btn" href="#">
+									<span class="btn-text">DETAILS</span>
+									<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+								</a>
+							</div>
+						</div>
+						<div class="el">
+							<div class="content">
+								<img class="icn" src="img/features/common-features/icn-mesurer.svg" alt="">
+								<h3>
+									Mesurer
+								</h3>
+								<a class="btn" href="#">
+									<span class="btn-text">DETAILS</span>
+									<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<?php include('common-sectionStart.php') ?> 
+		</main>
+
+		<footer></footer>
+
+		<script type="text/javascript" src="js/features.js"></script>
+		<script type="text/javascript" src="js/header.js"></script>
+	</body>
+</html>
