60c83796c735e29c70bc24c446a1eea22741d85e
Léonard <leonard@agence-me.com>
2018-11-21 18:05:58 -0500

    Pages Domaine

---
+++ Front/domaine-production.php
@@ -0,0 +1,219 @@
+<html>
+	<head>
+
+		<title>Domaine - Production</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/domaine.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php 
+			$styleHeader = "style-dark";
+			include('common-header.php'); 
+		?> 
+
+		<main class="domaine-production">
+
+			<section id="section-cover">
+				<img class="shape" src="img/home/sectionCover/shape.svg" alt="">
+				<div class="wrapper">
+					<div class="container-cover">
+						<div class="container-text">
+							<h1>
+								La planification de production simple et visuelle 
+							</h1>
+							<p>
+								Que vous souhaitiez savoir dans quel ordre et sur quelle machine traiter les commandes, quelles sont les tâches à accomplir ou combien de temps cela va-t-il prendre, utilisez notre solution de planification et d’ordonnancement sous contraintes, entièrement intégrée.
+							</p>
+						</div>
+						<div class="container-illu">
+							<img src="img/domaine/cover-production.png" alt="">
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-ftr">
+				<div class="wrapper">
+					<div class="container-text">
+						<h2>Comment ça marche ?</h2>
+						<p>
+							Far far away, behind the word mountains, far from the countries Vokalia and Consonantia
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el">
+							<div class="icn"></div>
+							<h3>
+								Définissez les besoins bruts
+							</h3>
+							<p>
+								Établissez un PDP (Plan directeur de production) à l’aide des données commerciales du PIC (Plan Industriel et Commercial) sur un horizon à court ou moyen terme et définissez les besoins bruts de produits finis à fabriquer. 
+							</p>
+						</div>
+						<div class="el">
+							<div class="icn"></div>
+							<h3>
+								Planifiez les ordres de travail et de fabrication
+							</h3>
+							<p>
+								Planifiez les ordres de travail et de fabrication saisis manuellement ou importés de votre ERP en fonction de la disponibilité des ressources et des composants et évoluez vers une demande Lean avec une gestion des flux en Juste à Temps (JIT) ou en dès que possible (ASAP).
+							</p>
+						</div>
+						<div class="el">
+							<div class="icn"></div>
+							<h3>
+								Équilibrez les charges de travail
+							</h3>
+							<p>
+								Elaborez un plan de charge en fonction des compétences et des capacités et optimisez l’utilisation de vos équipements et de vos effectifs (MRP - Manufacturing Resource Planning).
+							</p>
+						</div>
+						<div class="el">
+							<div class="icn"></div>
+							<h3>
+								Réduisez les Temps de Cycle (Turn around time)
+							</h3>
+							<p>
+								Anticipez les pics de charge en définissant un programme de production lissé et optimisé sur votre capacité du moment.  (Throughput)
+							</p>
+						</div>
+					</div>
+					<div class="container-btn">
+						<a href="">
+							<span class="btn-text">BUTTON WITH ICON</span>
+							<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-case">
+				<div class="wrapper">
+					<div class="container-text">
+						<div class="icn">
+							<img src="img/domaine/Lisi.png" alt="">
+						</div>
+						<div class="title">
+							CAS CLIENTS
+						</div>
+						<h3>
+							LISI Aerospace 
+						</h3>
+						<p>
+							LISI est un groupe international français (10 923 salariés, 43 sites de production dans le monde) spécialisé dans les composants et systèmes de fixation pour les secteurs automobile (LISI Automotive), médical (LISI Medical) et aérospatial (LISI Aerospace).
+						</p>
+						<a class="btn" href="#">
+							<span class="btn-text">LIRE LE BUSINESS CASE</span>
+							<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+						</a>
+					</div>
+					<div class="container-stats">
+						<div class="container-img" style="background-image: url(img/img-test/background-4.jpg);"></div>
+						<div class="container-el">
+							<div class="el">
+								<h4>ORDRES DE FABRICATION (OFS)</h4>
+								<div class="container-num">
+									<div class="num">
+										70 000
+									</div>
+								</div>
+								<p>
+									traités / an 
+								</p>
+							</div>
+							<div class="el">
+								<h4>LE ON TIME DELIVERY (OTD) EST PASSÉ DE</h4>
+								<div class="container-num">
+									<div class="num">
+										60 à 90
+									</div>
+									<div class="obj">
+										%
+									</div>
+								</div>
+								<p>
+									en quelques mois 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-avantage">
+				<div class="wrapper">
+					<div class="container-text">
+						<h2>Quels avantages ?</h2>
+						<p>
+							Far far away, behind the word mountains, far from the countries Vokalia and Consonantia
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el">
+							<div class="icn">
+								
+							</div>
+							<h3>
+								Augmentation du rendement
+							</h3>
+							<p>
+								La planification sous contraintes vous permet d’augmenter votre capacité de production sans ajouter de ressources et de minimiser les stocks.
+							</p>
+						</div>
+						<div class="el">
+							<div class="icn">
+								
+							</div>
+							<h3>
+								Augmentation de la satisfaction client
+							</h3>
+							<p>
+								Planilog vous aide à élever votre niveau de satisfaction client en réduisant les délais de livraison (On-Time Delivery) et en respectant les engagements pris à la commande.
+							</p>
+						</div>
+						<div class="el">
+							<div class="icn">
+								
+							</div>
+							<h3>
+								Baisse du besoin en fonds de roulement (BFR)
+							</h3>
+							<p>
+								La coordination des plannings de vente et de production vous offre une meilleure rotation des stocks et une accélération de votre flux de trésorerie. 
+							</p>
+						</div>
+						<div class="el">
+							<div class="icn">
+								
+							</div>
+							<h3>
+								Optimisation des actifs immobilisés
+							</h3>
+							<p>
+								Une planification synchronisée et actualisée en temps réel permet la maximisation du taux d’utilisation de vos actifs immobilisés.  
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<?php include('common-sectionStart.php') ?> 
+
+		</main>
+
+		<?php include('common-footer.php') ?> 
+
+		<script type="text/javascript" src="js/header.js"></script>
+	</body>
+</html>
