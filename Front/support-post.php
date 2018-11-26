60274169fcf01398516b3471bea3fd0cce2602d3
Léonard <leonard@agence-me.com>
2018-11-26 15:49:46 -0500

    Page Blog Post & Page Support Post

---
+++ Front/support-post.php
@@ -0,0 +1,210 @@
+<html>
+	<head>
+
+		<title>Support - Post</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/support-post.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php 
+			$styleHeader = "style-dark";
+			include('common-header.php'); 
+		?> 
+
+		<main>
+
+			<section id="section-cover">
+				<div class="wrapper">
+					<div class="container-title">
+						<div class="container-rslt">
+							<div class="text">
+								Compte et paramètres
+							</div>
+							<img src="img/support-list/arrow.svg" alt="">
+						</div>
+						<div class="container-dropdown">
+							<div class="container-el">							
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-settings.svg" alt="">
+									</div>
+									<div class="text">
+										Comptes et paramètres
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-flash.svg" alt="">
+									</div>
+									<div class="text">
+										Démarrage rapide
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-slider.svg" alt="">
+									</div>
+									<div class="text">
+										Configuration de Planilog
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-transfert.svg" alt="">
+									</div>
+									<div class="text">
+										Planilog Connect
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-star.svg" alt="">
+									</div>
+									<div class="text">
+										Fonctions premium
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-document.svg" alt="">
+									</div>
+									<div class="text">
+										Facturation et abonnement
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-lock.svg" alt="">
+									</div>
+									<div class="text">
+										Niveau de service et sécurité
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-search.svg" alt="">
+									</div>
+									<div class="text">
+										Glossaire
+									</div>
+								</a>
+								<a class="el">
+									<div class="icn">
+										<img src="img/support-list/icn-play.svg" alt="">
+									</div>
+									<div class="text">
+										Tutoriels vidéos
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-path">
+				<div class="wrapper">
+					<div class="container-el">
+						<a class="el" href="">Centre d’aide Planilog</a>
+						<img src="img/common/arrow-grey.svg" alt="">
+						<a class="el" href="">Compte et paramètres</a>
+						<img src="img/common/arrow-grey.svg" alt="">
+						<a class="el" href="">Mori nen tep</a>
+						<img src="img/common/arrow-grey.svg" alt="">
+						<a class="el" href="">Donec facilisis tortor ut augue lacinia, at vive.</a>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-post">
+				<div class="wrapper">
+					<p>
+						Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra 	   eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultrices. Donec euismod scelerisque ligula. <strong>Maecenas eu varius risus</strong>, eu aliquet arcu. Curabitur fermentum suscipit est, tincidunt mattis lorem luctus id. Donec eget massa a diam condimentum pretium. Aliquam erat volutpat. Integer ut tincidunt orci. Etiam tristique, elit ut consectetur iaculis, metus lectus mattis justo, vel mollis eros neque quis augue.
+						
+					</p>
+					<div class="container-quote">
+						<img class="obj" src="img/business/icn-quote.svg" alt="">
+						<p>
+							Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.
+						</p>
+						<div class="name">Tum Tantasatityanon</div>
+					</div>
+					<p>
+						Sed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut suscipit justo.Nam dapibus nisl vitae elit fringilla              , erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa. Vestibulum sed metus in lorem tristique ullamcorper id vitae erat. Nulla mollis sapien.
+					</p>
+					<img src="img/img-test/background-1.jpg" alt="">
+					<h2>Donec facilisis tortor ut augue lacinia, at vive.</h2>
+					<p>
+						ed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut suscipit justo.Nam dapibus nisl vitae elit fringilla rutrum, erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa. Vestibulum sed metus in lorem tristique ullamcorper id vitae erat. Nulla mollis sapien.
+					</p>
+					<ul>
+						<li>Fusce vehicula</li>
+						<li>dolor arcu</li>
+						<li>sit amet blandit</li>
+						<li>dolor mollis nec</li>
+						<li>Donec viverra eleifend</li>
+						<li>lacus, vitae ullamcorper</li>
+						<li>metus. Sed sollicitudin</li>
+						<li>ipsum quis nunc</li>
+						<li>sollicitudin ultrices</li>
+					</ul>
+					<h3>Donec facilisis tortor ut augue lacinia, at vive.</h3>
+					<p>
+						ed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut suscipit justo.Nam dapibus nisl vitae elit fringilla rutrum, erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa. Vestibulum sed metus in lorem tristique ullamcorper id vitae erat. Nulla mollis sapien.
+					</p>
+					<button>
+						<span class="btn-text">BOUTON</span>
+						<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+					</button>
+					<iframe src="https://www.youtube.com/embed/1A-OZxCr-Po" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</section>
+
+			<section id="section-relatedArticles">
+				<div class="wrapper">
+					<div class="container-col">
+						<div class="col">
+							<h3>Articles Associés</h3>
+							<ul>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>	
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+							</ul>
+						</div>
+						<div class="col">
+							<h3>Articles consultés récemment</h3>
+							<ul>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+								<li><a href="">Donec facilisis tortor ut augue lacinia, at vive.</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			<?php include('common-sectionBanner.php') ?> 
+
+		</main>
+
+		<?php include('common-footer.php') ?> 
+
+		<script type="text/javascript" src="js/support-post.js"></script>
+		<script type="text/javascript" src="js/header.js"></script>
+	</body>
+</html>
