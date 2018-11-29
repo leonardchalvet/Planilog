537fc6afe49a1af2a82e91eec571954a67bf77b3
Léonard <leonard@agence-me.com>
2018-11-29 15:29:12 -0500

    Tooltip + link post

--- Front/js/post.js
+++ Front/js/post.js
@@ -1,10 +1,10 @@
 $(window).on('load', function() {
 
 	$('.rs-link').click(function(){
-		$('.rs-link .dropdown').fadeIn(250);
+		$(this).find('.dropdown').fadeIn(250);
 		setTimeout(function() {
 			$('.rs-link .dropdown').fadeOut(250);
-		}, 4000);
+		}, 3000);
 	})
 
 })
