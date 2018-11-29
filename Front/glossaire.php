ea5061658b54ea81d973707766300eb168b31293
Léonard <leonard@agence-me.com>
2018-11-29 16:44:08 -0500

    Glossaire

--- Front/glossaire.php
+++ Front/glossaire.php
@@ -28,14 +28,139 @@
 				<div class="wrapper">
 					<div class="container-list">
 						<div class="container-input">
-							<img src="" alt="">
-							<input type="text">
+							<div class="icn">
+								<img src="img/glossaire/search.svg" alt="">
+							</div>
+							<input type="text" placeholder="Rechercher">
 						</div>
+						<div class="container-el">
+							<ul>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+								<li>
+									Autwock meh
+								</li>
+							</ul>
+						</div>
+						
+					</div>
+					<div class="container-text">
+						<h1>Autwock meh</h1>
+						<p>
+							Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra 	   eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultrices. Donec euismod scelerisque ligula. <strong>Maecenas eu varius risus</strong>, eu aliquet arcu. Curabitur fermentum suscipit est, tincidunt mattis lorem luctus id. Donec eget massa a diam condimentum pretium. Aliquam erat volutpat. Integer ut tincidunt orci. Etiam tristique, elit ut consectetur iaculis, metus lectus mattis justo, vel mollis eros neque quis augue.
+							
+						</p>
+						<div class="container-quote">
+							<img class="obj" src="img/business/icn-quote.svg" alt="">
+							<p>
+								Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.
+							</p>
+							<div class="name">Tum Tantasatityanon</div>
+						</div>
+						<p>
+							Sed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut suscipit justo.Nam dapibus nisl vitae elit fringilla              , erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa. Vestibulum sed metus in lorem tristique ullamcorper id vitae erat. Nulla mollis sapien.
+							<img src="img/img-test/background-1.jpg" alt="">
+						</p>
+						
+						<h2>Donec facilisis tortor ut augue lacinia, at vive.</h2>
+						<p>
+							ed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut suscipit justo.Nam dapibus nisl vitae elit fringilla rutrum, erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa. Vestibulum sed metus in lorem tristique ullamcorper id vitae erat. Nulla mollis sapien.
+						</p>
 						<ul>
-							<li>
-								
-							</li>
+							<li>Fusce vehicula</li>
+							<li>dolor arcu</li>
+							<li>sit amet blandit</li>
+							<li>dolor mollis nec</li>
+							<li>Donec viverra eleifend</li>
+							<li>lacus, vitae ullamcorper</li>
+							<li>metus. Sed sollicitudin</li>
+							<li>ipsum quis nunc</li>
+							<li>sollicitudin ultrices</li>
 						</ul>
+						<h3>Donec facilisis tortor ut augue lacinia, at vive.</h3>
+						<p>
+							ed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut suscipit justo.Nam dapibus nisl vitae elit fringilla rutrum, erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa. Vestibulum sed metus in lorem tristique ullamcorper id vitae erat. Nulla mollis sapien.
+						</p>
+						<button>
+							<span class="btn-text">BOUTON</span>
+							<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+						</button>
+						<iframe src="https://www.youtube.com/embed/1A-OZxCr-Po" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<div class="container-rs">
+							<a href="">
+								<img src="img/blog/icn-facebook.svg" alt="">
+							</a>
+							<a href="">
+								<img src="img/blog/icn-twitter.svg" alt="">
+							</a>
+							<a href="">
+								<img src="img/blog/icn-linkedin.svg" alt="">
+							</a>
+							<a class="rs-link">
+								<img src="img/blog/icn-hyperlink.svg" alt="">
+								<span class="dropdown">
+									votre lien a été copié
+								</span>
+							</a>
+						</div>
 					</div>
 				</div>
 			</section>
