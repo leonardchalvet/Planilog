b771bfcd4d67c79a0a540b858b6c4615e803f5c4
Léonard <leonard@agence-me.com>
2018-12-07 11:21:01 +0100

    Banner Cookie, Banner notifs, page cookies, WYSIWYG bug fix

---
+++ Front/cookies.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+
+		<title>Cookies</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/cookies.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?> 
+
+		<main>
+
+			<section id="section-cookies">
+				<div class="wrapper">
+					<h2>Privacy Policy</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus repellendus, <a href="">voluptate</a> laborum magnam odit aliquid officiis necessitatibus ab veniam, perferendis magni eligendi voluptas, sint delectus maiores. Ullam doloremque fuga eveniet.
+					</p>
+					<a href="" class="btn">
+						<span class="btn-text">
+							REFUSER
+						</span>
+					</a>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?> 
+
+		<script type="text/javascript" src="js/header.js"></script>
+	</body>
+</html>
