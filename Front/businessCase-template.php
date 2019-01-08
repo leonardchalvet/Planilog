5fa8407865fe668916da858c29564a687f0a8360
Léonard <leonard@agence-me.com>
2019-01-08 17:30:36 +0100

    New businesscase template

---
+++ Front/businessCase-template.php
@@ -0,0 +1,140 @@
+<html>
+	<head>
+
+		<title>Business Case - Services</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/businesscase-template.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?> 
+
+		<main>
+
+			<section id="section-pres">
+				<img class="shape" src="img/home/sectionCover/shape.svg" alt="">
+				<div class="wrapper">
+					<h2>CAS CLIENTS</h2>
+					<h1>LISI Aerospace </h1>
+					<p>
+						Optimiser l’ordonnancement des flux pour améliorer l’On Time Delivery
+					</p>
+					<a href="" class="btn">
+						<span class="btn-text">
+							VOIR LE SITE
+						</span>
+						<img class="btn-arrow" src="img/business/arrow.svg" alt="">
+					</a>
+				</div>
+			</section>
+
+			<section id="section-el">
+				<div class="wrapper">
+					<div class="container-el">
+						<div class="el">
+							<div class="container-text">
+								<h3>Le contexte LISI Aerospace</h3>
+								<p>
+									LISI est un groupe international français (10 923 salariés, 43 sites de production dans le monde) spécialisé dans les composants et systèmes de fixation pour les secteurs automobile (LISI Automotive), médical (LISI Medical) et aérospatial (LISI Aerospace).<br> 
+									<br> 
+									LISI Aerospace est une entreprise mondiale active sur le marché de l'aéronautique depuis 1950 avec plus de 300 clients dans 30 pays. Elle est évaluée à plus de 1,3 milliard de dollars. Elle conçoit, fabrique et fournit des fixations et des composants d’assemblage et de structure pour les plus grands acteurs mondiaux du secteur. Avec des composants et sous-ensembles à fort contenu technique, elle fait la différence dans sa capacité à innover et à maintenir des normes de performance opérationnelle élevées.<br> 
+									<br> 
+									LISI Aerospace est en mesure de répondre à tout type de besoin d'assemblage, de l'avant-projet à la conception, de la fabrication à la livraison de composants ou sous-ensembles primaires, dans un cadre de gestion de programme.
+								</p>
+							</div>
+							<div class="container-obj">
+								<div class="obj-img" style="background-image: url(img/img-test/background-4.jpg);">
+									
+								</div>
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="container-text">
+								<h3>Le contexte LISI Aerospace</h3>
+								<p>
+									LISI est un groupe international français (10 923 salariés, 43 sites de production dans le monde) spécialisé dans les composants et systèmes de fixation pour les secteurs automobile (LISI Automotive), médical (LISI Medical) et aérospatial (LISI Aerospace).<br> 
+									<br> 
+									LISI Aerospace est une entreprise mondiale active sur le marché de l'aéronautique depuis 1950 avec plus de 300 clients dans 30 pays. Elle est évaluée à plus de 1,3 milliard de dollars. Elle conçoit, fabrique et fournit des fixations et des composants d’assemblage et de structure pour les plus grands acteurs mondiaux du secteur. Avec des composants et sous-ensembles à fort contenu technique, elle fait la différence dans sa capacité à innover et à maintenir des normes de performance opérationnelle élevées.<br> 
+									<br> 
+									LISI Aerospace est en mesure de répondre à tout type de besoin d'assemblage, de l'avant-projet à la conception, de la fabrication à la livraison de composants ou sous-ensembles primaires, dans un cadre de gestion de programme.
+								</p>
+							</div>
+							<div class="container-obj">
+								<div class="obj-num">
+									<div class="container-el">
+										<div class="el">
+											<h4>
+												ORDRES DE FABRICATION (OFS) 
+											</h4>
+											<div class="num">
+												70 000
+											</div>
+											<div class="desc">
+												traités / an 
+											</div>
+										</div>
+										<div class="el">
+											<h4>
+												LE ON TIME DELIVERY (OTD) EST PASSÉ DE 
+											</h4>
+											<div class="num">
+												60 à 90 %
+											</div>
+											<div class="desc">
+												en quelques mois 
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="container-text">
+								<h3>Le contexte LISI Aerospace</h3>
+								<p>
+									LISI est un groupe international français (10 923 salariés, 43 sites de production dans le monde) spécialisé dans les composants et systèmes de fixation pour les secteurs automobile (LISI Automotive), médical (LISI Medical) et aérospatial (LISI Aerospace).<br> 
+									<br> 
+									LISI Aerospace est une entreprise mondiale active sur le marché de l'aéronautique depuis 1950 avec plus de 300 clients dans 30 pays. Elle est évaluée à plus de 1,3 milliard de dollars. Elle conçoit, fabrique et fournit des fixations et des composants d’assemblage et de structure pour les plus grands acteurs mondiaux du secteur. Avec des composants et sous-ensembles à fort contenu technique, elle fait la différence dans sa capacité à innover et à maintenir des normes de performance opérationnelle élevées.<br> 
+									<br> 
+									LISI Aerospace est en mesure de répondre à tout type de besoin d'assemblage, de l'avant-projet à la conception, de la fabrication à la livraison de composants ou sous-ensembles primaires, dans un cadre de gestion de programme.
+								</p>
+							</div>
+							<div class="container-obj">
+								<div class="obj-quote">
+									<img src="img/business/icn-quote.svg" alt="">
+									<div class="quote">
+										Planilog nous a permis de gagner plusieurs mois de capacité de production par an, sans augmenter nos ressources 
+									</div>
+									<div class="name">
+										Tum Tantasatityanon
+									</div>
+									<div class="post">
+										Lorem
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?> 
+
+		<script type="text/javascript" src="js/header.js"></script>
+	</body>
+</html>
