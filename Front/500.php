e448b111f430f763e839a79e8ea4861607eb8cd0
Léonard <leonard@agence-me.com>
2019-01-09 11:02:35 +0100

    Page 404 & 500

---
+++ Front/500.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+
+		<title>500</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/lost.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<main>
+
+
+			<div class="head">
+				<div class="wrapper">
+					<a href="">
+						<img src="img/common/logo.png" alt="">
+					</a>
+				</div>
+			</div>
+
+
+			<section id="section-page">
+
+				<img class="shape" src="img/home/sectionCover/shape.svg" alt="">
+
+				<div class="wrapper">
+					<h1>500</h1>
+					<p>la page que vous recherchez semble introuvable</p>
+					<a class="btn" href="#">
+						<div class="btn-text">ACCUEIL</div>
+						<img class="btn-arrow" src="img/common/arrow-white.svg" alt="">
+					</a>
+				</div>
+				
+			</section>
+
+
+		</main>
+
+		<script type="text/javascript" src="js/header.js"></script>
+	</body>
+</html>
