1b3aff22dfe2b60d303fa774b8efa7631d1ae7e6
Léonard <leonard@agence-me.com>
2019-02-04 22:33:20 -0500

    Add input contact commercial

--- Front/contact-commercial.php
+++ Front/contact-commercial.php
@@ -73,7 +73,8 @@
 						<div class="container-rcl">
 							<div class="title">Vous souhaitez être recontacté par :</div>
 							<div class="container-radio">
-								<div class="radio">
+								<input id="input-radio" type="hidden">
+								<div class="radio" data-radio="E-mail">	
 									<div class="content">
 										<div class="btn-radio">
 											<div class="rd"></div>
@@ -81,7 +82,7 @@
 										<div class="text">E-mail</div>
 									</div>
 								</div>
-								<div class="radio">
+								<div class="radio" data-radio="Téléphone">
 									<div class="content">
 										<div class="btn-radio">
 											<div class="rd"></div>
@@ -89,13 +90,14 @@
 										<div class="text">Téléphone</div>
 									</div>
 									<div class="container-checkbox">
-										<div class="checkbox">
+										<input id="input-checkbox" type="hidden">
+										<div class="checkbox" data-checkbox="Matin">
 											<div class="checkbox-btn">
 												<img src="img/contact-commercial/check.svg" alt="">
 											</div>
 											<div class="text">Matin</div>
 										</div>
-										<div class="checkbox">
+										<div class="checkbox" data-checkbox="Après midi">
 											<div class="checkbox-btn">
 												<img src="img/contact-commercial/check.svg" alt="">
 											</div>
--- Front/js/contact-commercial.js
+++ Front/js/contact-commercial.js
@@ -1,13 +1,19 @@
 $(window).on('load', function() {
 
 	$('#section-contact form .container-rcl .container-radio .radio').click(function(){
+		var elval = $(this).data('radio');
+		$('#input-radio').val(elval);
 		$('#section-contact form .container-rcl .container-radio .radio').removeClass('active');
 		$(this).addClass('active');
 	})
 
 	$('#section-contact form .container-rcl .container-radio .radio .container-checkbox .checkbox').click(function(){
+		var elval = $(this).data('checkbox');
+		$('#input-checkbox').val(elval);
 		$('#section-contact form .container-rcl .container-radio .radio .container-checkbox .checkbox').removeClass('active');
 		$(this).addClass('active');
 	})
 
 })
+
+
