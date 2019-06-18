5a4325b2111a3f3d68a8344e0cad11ad86d37cc3
maxgodenne <51903667+maxgodenne@users.noreply.github.com>
2019-06-18 04:15:59 +0200

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -51,6 +51,56 @@
     @endforeach
     <link rel="canonical" href="{{ LaravelLocalization::getLocalizedURL() }}">
 
+<!-- Start of Async Drift Code -->
+<script>
+"use strict";
+
+!function() {
+  var t = window.driftt = window.drift = window.driftt || [];
+  if (!t.init) {
+    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+    t.factory = function(e) {
+      return function() {
+        var n = Array.prototype.slice.call(arguments);
+        return n.unshift(e), t.push(n), t;
+      };
+    }, t.methods.forEach(function(e) {
+      t[e] = t.factory(e);
+    }), t.load = function(t) {
+      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+      var i = document.getElementsByTagName("script")[0];
+      i.parentNode.insertBefore(o, i);
+    };
+  }
+}();
+drift.SNIPPET_VERSION = '0.3.1';
+drift.load('m3z94n6mcnm7');
+</script>
+<!-- End of Async Drift Code -->
+
+
+<!-- Hotjar Tracking Code for www.planilog.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1365974,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+<!-- END: Hotjar -->
+
+<!-- Crisp -->
+<script type="text/javascript">
+window.$crisp=[];window.CRISP_WEBSITE_ID="8edf0053-091e-4629-81d7-2c2ca36066b9";
+(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";
+s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+</script>
+<!-- END: Crisp -->
 
 </head>
 
@@ -72,18 +122,7 @@
 @include('components.header_cookies')
 
 
-<!-- Hotjar Tracking Code for www.planilog.com -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1365974,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-<!-- END: Hotjar -->
+
 
 <!-- ActiveCampaign Conversation -->
 <script type="text/javascript">
@@ -95,13 +134,11 @@
 </script>
 <!-- END: ActiveCampaign -->
 
-<!-- Crisp -->
-<script type="text/javascript">
-window.$crisp=[];window.CRISP_WEBSITE_ID="8edf0053-091e-4629-81d7-2c2ca36066b9";
-(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";
-s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-</script>
-<!-- END: Crisp -->
+
+
+
+
+
 
 <!-- Albacross -->
 <script type="text/javascript">
