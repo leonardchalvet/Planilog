30739a9ac5bf94cc545bfa261ca676b89bdb0987
maxgodenne <51903667+maxgodenne@users.noreply.github.com>
2019-07-17 10:08:51 +0200

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -73,6 +73,12 @@
 </script>
 <!-- END: Hotjar -->
 
+<!-- Oribi Tracking Code -->
+<script type="application/javascript">
+  (function(b, o, n, g, s, r, c) { if (b[s]) return; b[s] = {}; b[s].scriptToken = "XzE4NTg3MjY5NDk"; b[s].callsQueue = []; b[s].api = function() { b[s].callsQueue.push(arguments); }; r = o.createElement(n); c = o.getElementsByTagName(n)[0]; r.async = 1; r.src = g; r.id = s + n; c.parentNode.insertBefore(r, c); })(window, document, "script", "//cdn.oribi.io/XzE4NTg3MjY5NDk/oribi.js", "ORIBI");
+</script>
+<!-- END: Oribi -->
+
 </head>
 
 <body>
