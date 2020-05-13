d4f93d9d94fa2e21c71b04495d00d86f6afdd1af
Léonard <leonard@agence-me.com>
2020-05-13 17:28:13 +0200

    Change - Front - Header Desktop & Mobile - Update CSS

--- Front/common-header.php
+++ Front/common-header.php
@@ -96,14 +96,9 @@
 					</div>
 				</div>
 			</li>
-			<li>
-				<div class="container-text">
-					<a href="#" class="title">Tarifs</a>
-				</div>
-			</li>
 			<li class="li-dropdown li-dropdown-3">
 				<div class="container-text">
-					<div class="title">Centre d’aide</div>
+					<div class="title">Intégrations</div>
 					<img class="arrow" src="img/common/arrow-red.svg" alt="">
 				</div>
 				<div class="dropdown">
@@ -160,6 +155,151 @@
 									<h3>Facturation et abonnement</h3>
 									<p>
 										Tout ce dont vous avez besoin de savoir à propos de l’importation, l’exportation et la migration de données.              
+									</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			
+			<li class="li-dropdown li-dropdown-4">
+				<div class="container-text">
+					<div class="title">Tarifs</div>
+					<img class="arrow" src="img/common/arrow-red.svg" alt="">
+				</div>
+				<div class="dropdown">
+					<div class="content">
+						<div class="container-el">
+							<a class="el" href="features-planifier.php">
+								<img class="icn" src="img/features/common-features/icn-planifier.svg" alt="">
+								<div class="text">
+									<h3>Planifier</h3>
+									<p>
+										Élaborez un planning conforme à l’ensemble de vos règles métiers, de la réglementation en vigueur et de vos objectifs.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-simuler.php">
+								<img class="icn" src="img/features/common-features/icn-simuler.svg" alt="">
+								<div class="text">
+									<h3>Simuler</h3>
+									<p>
+										Créez des scénarios d’optimisation et visualisez les conséquences de chacune de vos décisions dans une logique d’amélioration continue.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-ordonnancer.php">
+								<img class="icn" src="img/features/common-features/icn-ordonnancer.svg" alt="">
+								<div class="text">
+									<h3>Ordonnancer</h3>
+									<p>
+										Ajustez votre planning en tenant compte de vos contraintes d’ordonnancement qu’il s’agisse de vos machines, de vos ressources humaines ou des besoins clients.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-mesurer.php">
+								<img class="icn" src="img/features/common-features/icn-mesurer.svg" alt="">
+								<div class="text">
+									<h3>Mesurer</h3>
+									<p>
+										Évaluez la qualité de vos plannings en vous basant sur vos indicateurs de performance opérationnelle (KPI) .
+									</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="li-dropdown li-dropdown-5">
+				<div class="container-text">
+					<div class="title">Partenaires</div>
+					<img class="arrow" src="img/common/arrow-red.svg" alt="">
+				</div>
+				<div class="dropdown">
+					<div class="content">
+						<div class="container-el">
+							<a class="el" href="features-planifier.php">
+								<img class="icn" src="img/features/common-features/icn-planifier.svg" alt="">
+								<div class="text">
+									<h3>Planifier</h3>
+									<p>
+										Élaborez un planning conforme à l’ensemble de vos règles métiers, de la réglementation en vigueur et de vos objectifs.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-simuler.php">
+								<img class="icn" src="img/features/common-features/icn-simuler.svg" alt="">
+								<div class="text">
+									<h3>Simuler</h3>
+									<p>
+										Créez des scénarios d’optimisation et visualisez les conséquences de chacune de vos décisions dans une logique d’amélioration continue.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-ordonnancer.php">
+								<img class="icn" src="img/features/common-features/icn-ordonnancer.svg" alt="">
+								<div class="text">
+									<h3>Ordonnancer</h3>
+									<p>
+										Ajustez votre planning en tenant compte de vos contraintes d’ordonnancement qu’il s’agisse de vos machines, de vos ressources humaines ou des besoins clients.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-mesurer.php">
+								<img class="icn" src="img/features/common-features/icn-mesurer.svg" alt="">
+								<div class="text">
+									<h3>Mesurer</h3>
+									<p>
+										Évaluez la qualité de vos plannings en vous basant sur vos indicateurs de performance opérationnelle (KPI) .
+									</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="li-dropdown li-dropdown-6">
+				<div class="container-text">
+					<div class="title">Centre d'aide</div>
+					<img class="arrow" src="img/common/arrow-red.svg" alt="">
+				</div>
+				<div class="dropdown">
+					<div class="content">
+						<div class="container-el">
+							<a class="el" href="features-planifier.php">
+								<img class="icn" src="img/features/common-features/icn-planifier.svg" alt="">
+								<div class="text">
+									<h3>Planifier</h3>
+									<p>
+										Élaborez un planning conforme à l’ensemble de vos règles métiers, de la réglementation en vigueur et de vos objectifs.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-simuler.php">
+								<img class="icn" src="img/features/common-features/icn-simuler.svg" alt="">
+								<div class="text">
+									<h3>Simuler</h3>
+									<p>
+										Créez des scénarios d’optimisation et visualisez les conséquences de chacune de vos décisions dans une logique d’amélioration continue.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-ordonnancer.php">
+								<img class="icn" src="img/features/common-features/icn-ordonnancer.svg" alt="">
+								<div class="text">
+									<h3>Ordonnancer</h3>
+									<p>
+										Ajustez votre planning en tenant compte de vos contraintes d’ordonnancement qu’il s’agisse de vos machines, de vos ressources humaines ou des besoins clients.
+									</p>
+								</div>
+							</a>
+							<a class="el" href="features-mesurer.php">
+								<img class="icn" src="img/features/common-features/icn-mesurer.svg" alt="">
+								<div class="text">
+									<h3>Mesurer</h3>
+									<p>
+										Évaluez la qualité de vos plannings en vous basant sur vos indicateurs de performance opérationnelle (KPI) .
 									</p>
 								</div>
 							</a>
